bc458c7f38301587b2ccedf80f3beccd8c7462a0
Alexander Chernoff <alex@chernoff.xyz>
2025-05-17 11:37:47 -0400

    add iOS and visionOS support

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.3
+// swift-tools-version:6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
   name: "lua4swift",
+  platforms: [.macOS(.v10_13), .iOS(.v12), .visionOS(.v1)],
   products: [
     .library(
       name: "lua4swift",
@@ -16,7 +17,11 @@ let package = Package(
     .target(
         name: "CLua",
         dependencies: [],
-        path: "Sources/CLua"),
+        path: "Sources/CLua",
+        cSettings: [
+            .define("LUA_USE_MACOSX", .when(platforms: [.macOS])),
+            .define("LUA_USE_IOS", .when(platforms: [.iOS, .visionOS]))
+        ]),
     .target(
       name: "lua4swift",
       dependencies: [
